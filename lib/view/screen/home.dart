96f93d9e853be7197eb14809cb5c9d6638b01279
zaidoun92 <27ew75ma@gmail.com>
2023-03-28 11:55:51 +0300

    Home Page Design ui part 1

--- lib/view/screen/home.dart
+++ lib/view/screen/home.dart
@@ -9,11 +9,49 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     HomeControllerImp controller = Get.put(HomeControllerImp());
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("HomePage"),
-      ),
-      body: Center(
-        child: Text(controller.id!),
+      body: ListView(
+        children: [
+          Container(
+            margin: const EdgeInsets.only(top: 10),
+            padding: const EdgeInsets.symmetric(horizontal: 15),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      prefixIcon: const Icon(Icons.search),
+                      hintText: "Find Product",
+                      hintStyle: const TextStyle(fontSize: 18),
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide.none,
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      filled: true,
+                      fillColor: Colors.grey[200],
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 10),
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.grey[200],
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  width: 60,
+                  padding: const EdgeInsets.symmetric(vertical: 8),
+                  child: IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.notifications_active_outlined,
+                      size: 30,
+                      color: Colors.grey[600],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
