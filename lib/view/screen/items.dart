c757106178f0342e093797348d2756b6109ad8e3
zaidoun92 <27ew75ma@gmail.com>
2023-04-09 11:50:27 +0300

    Item Page Ui part 3

--- lib/view/screen/items.dart
+++ lib/view/screen/items.dart
@@ -1,3 +1,6 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:ecommercecourse/core/constant/color.dart';
+import 'package:ecommercecourse/linkapi.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import '../../controller/items_controller.dart';
@@ -22,6 +25,63 @@ class Items extends StatelessWidget {
             ),
             const SizedBox(height: 20),
             const CustomListCategoriesItems(),
+            GridView.builder(
+              shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
+              itemCount: 2,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                childAspectRatio: 0.6,
+              ),
+              itemBuilder: (BuildContext context, int index) {
+                return InkWell(
+                  child: Card(
+                    child: Padding(
+                      padding: const EdgeInsets.all(10),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          CachedNetworkImage(
+                              imageUrl: "${AppLink.imageItems}/laptop.png"),
+                          const Text(
+                            "Laptop Surface Go",
+                            style: TextStyle(
+                              color: AppColor.black,
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          const Text(
+                            "Description how are you zaidoun",
+                            textAlign: TextAlign.center,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              const Text(
+                                "200.10 \$",
+                                style: TextStyle(
+                                    color: AppColor.primaryColor,
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold,
+                                    fontFamily: "sans"),
+                              ),
+                              IconButton(
+                                onPressed: () {},
+                                icon: const Icon(
+                                  Icons.favorite,
+                                  color: AppColor.primaryColor,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
           ],
         ),
       ),
